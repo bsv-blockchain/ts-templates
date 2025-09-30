4c691d20f86a230ea96e5297c3b4af39fd22ddf4
Deggen <d.kellenschwiler@bsvassociation.org>
2025-09-30 14:16:52 -0500

    create tests

---
+++ tests/MultiSigPubkeyHash.test.ts
@@ -0,0 +1,67 @@
+import { describe, it, expect } from 'vitest';
+import { Transaction, UnlockingScript, MerklePath, P2PKH, PrivateKey } from '@bsv/sdk';
+import { MockChain, wallet, makeWallet } from './test-utils';
+import { MultiSigPubkeyHash } from '../src/MultiSigPubkeyHash';
+
+const key = PrivateKey.fromRandom()
+
+describe('MultiSigPubkeyHash', () => {
+    it('should create a valid locking script for MultiSigPubkeyHash and unlock it', async () => {
+        
+        const wallet = await makeWallet('main', 'https://storage.babbage.systems', PrivateKey.fromRandom().toString())
+        const player1 = await makeWallet('main', 'https://storage.babbage.systems', PrivateKey.fromRandom().toString())
+        const player2 = await makeWallet('main', 'https://storage.babbage.systems', PrivateKey.fromRandom().toString())
+        const player3 = await makeWallet('main', 'https://storage.babbage.systems', PrivateKey.fromRandom().toString())
+        const players = [player1, player2, player3]
+
+        const { publicKey: counterparty } = await wallet.getPublicKey({ identityKey: true })
+
+        const publicKeys = await Promise.all(players.map(async player => {
+            const response = await player.getPublicKey({ identityKey: true })
+            return response.publicKey
+        }))
+
+        const keyID = new Date().toISOString()
+        const multiSigAddress = await MultiSigPubkeyHash.addressBRC29(wallet, publicKeys, keyID, 1)
+
+        const sourceTransaction = new Transaction()
+        sourceTransaction.addInput({
+            sourceTXID: '0000000000000000000000000000000000000000000000000000000000000000',
+            sourceOutputIndex: 0,
+            unlockingScript: UnlockingScript.fromASM('01')
+        })
+        sourceTransaction.addOutput({
+            satoshis: 1,
+            lockingScript: new MultiSigPubkeyHash().lock(multiSigAddress)
+        })
+        sourceTransaction.addOutput({
+            satoshis: 30,
+            lockingScript: new P2PKH().lock(key.toAddress())
+        })
+    
+        const txid = sourceTransaction.id('hex')
+        sourceTransaction.merklePath = new MerklePath(0, [[{ txid: true, offset: 0, hash: txid }]])
+        const mockChain = new MockChain({ blockheaders: [txid] })
+    
+        const tx = new Transaction()
+        tx.addInput({
+            sourceTransaction,
+            sourceOutputIndex: 0,
+            unlockingScriptTemplate: new MultiSigPubkeyHash().unlock(player1, { keyID, counterparty, forSelf: true })
+        })
+        tx.addInput({
+            sourceTransaction,
+            sourceOutputIndex: 1,
+            unlockingScriptTemplate: new P2PKH().unlock(key)
+        })
+        tx.addOutput({
+            change: true,
+            lockingScript: new P2PKH().lock(key.toAddress())
+        })
+        await tx.fee()
+        await tx.sign()
+
+        const passes = await tx.verify(mockChain)
+        expect(passes).toBe(true)
+    })
+})
---
+++ tests/test-utils.ts
@@ -0,0 +1,41 @@
+import { PrivateKey, KeyDeriver, WalletInterface, ChainTracker } from '@bsv/sdk'
+import { Wallet, WalletStorageManager, WalletSigner, Services, StorageClient } from '@bsv/wallet-toolbox'
+import { config } from 'dotenv'
+config()
+
+const PRIVATE_KEY: string = process.env.PRIVATE_KEY || ''
+
+export async function makeWallet (
+  chain: 'test' | 'main' = 'main',
+  storageURL: string = 'https://storage.babbage.systems',
+  privateKey: string = PRIVATE_KEY
+): Promise<WalletInterface> {
+  const keyDeriver = new KeyDeriver(new PrivateKey(privateKey, 'hex'))
+  const storageManager = new WalletStorageManager(keyDeriver.identityKey)
+  const signer = new WalletSigner(chain, keyDeriver, storageManager)
+  const services = new Services(chain)
+  const wallet = new Wallet(signer, services)
+  const client = new StorageClient(
+    wallet,
+    storageURL
+  )
+  await client.makeAvailable()
+  await storageManager.addWalletStorageProvider(client)
+
+  return wallet
+}
+
+export const wallet = await makeWallet()
+
+export class MockChain implements ChainTracker {
+  mock: { blockheaders: [string] }
+  constructor(mock: { blockheaders: [string] }) {
+    this.mock = mock
+  }
+  async isValidRootForHeight(root: string, height: number): Promise<boolean> {
+    return this.mock.blockheaders[height] === root
+  }
+  async currentHeight(): Promise<number> {
+    return this.mock.blockheaders.length
+  }
+}
