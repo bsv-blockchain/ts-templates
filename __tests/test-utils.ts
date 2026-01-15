328b32cb63a9003f8f5945eae55658ec65832f31
Deggen <d.kellenschwiler@bsvassociation.org>
2026-01-15 14:26:06 -0600

    keep local
    Signed-off-by: Deggen <d.kellenschwiler@bsvassociation.org>

--- __tests/test-utils.ts
+++ __tests/test-utils.ts
@@ -1,24 +1,10 @@
-import { PrivateKey, KeyDeriver, WalletInterface, ChainTracker } from '@bsv/sdk'
-import { Wallet, WalletStorageManager, WalletSigner, Services, StorageClient } from '@bsv/wallet-toolbox'
+import { PrivateKey, WalletInterface, ChainTracker, ProtoWallet } from '@bsv/sdk'
 
 export async function makeWallet (
-  chain: 'test' | 'main' = 'main',
-  storageURL: string = 'https://store-us-1.bsvb.tech',
   privateKey: string = PrivateKey.fromRandom().toString()
 ): Promise<WalletInterface> {
-  const keyDeriver = new KeyDeriver(new PrivateKey(privateKey, 'hex'))
-  const storageManager = new WalletStorageManager(keyDeriver.identityKey)
-  const signer = new WalletSigner(chain, keyDeriver, storageManager)
-  const services = new Services(chain)
-  const wallet = new Wallet(signer, services)
-  const client = new StorageClient(
-    wallet,
-    storageURL
-  )
-  await client.makeAvailable()
-  await storageManager.addWalletStorageProvider(client)
-
-  return wallet
+  const wallet = new ProtoWallet(PrivateKey.fromString(privateKey))
+  return wallet as WalletInterface
 }
 
 export class MockChain implements ChainTracker {
